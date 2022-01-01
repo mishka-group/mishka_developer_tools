01cdebb5f4c19721ed4d86c6d4b4335327bb6279
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-01-01 21:11:38 +0330

    delete badEnv

--- lib/repo.ex
+++ lib/repo.ex
@@ -1,9 +1,5 @@
-if Mix.env() in [:test, :dev] do
-  defmodule MishkaDeveloperTools.Repo do
-    use Ecto.Repo,
-      otp_app: :mishka_developer_tools,
-      adapter: Ecto.Adapters.Postgres
-  end
-else
-  raise BadEnv, message: "You can not use it as a prod package"
+defmodule MishkaDeveloperTools.Repo do
+  use Ecto.Repo,
+    otp_app: :mishka_developer_tools,
+    adapter: Ecto.Adapters.Postgres
 end
