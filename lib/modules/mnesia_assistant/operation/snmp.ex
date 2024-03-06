42e80cb1d632b8d940f8ee21bd1a9ffa4f84bdfc
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-03-06 20:30:59 +0330

    Add entry docs for Snmp, based on #28

--- lib/modules/mnesia_assistant/operation/snmp.ex
+++ lib/modules/mnesia_assistant/operation/snmp.ex
@@ -1,34 +1,38 @@
 defmodule MnesiaAssistant.Snmp do
   @moduledoc """
-
+  This module provides Simple Network Management Protocol (SNMP) functionality for mnesia.
+  > TODO: This module needs to completely cover all of the functions that are
+  > relevant to it.
+  > In the event that you have mastered this section, kindly submit a Pull Request
+  > for this section.
   """
   alias :mnesia, as: Mnesia
 
   @doc """
-
+  TODO: Neet to be completed.
   """
   def snmp_close_table(table), do: Mnesia.snmp_close_table(table)
 
   @doc """
-
+  TODO: Neet to be completed.
   """
   def snmp_get_mnesia_key(table, rowIndex) when is_list(rowIndex),
     do: Mnesia.snmp_get_mnesia_key(table, rowIndex)
 
   @doc """
-
+  TODO: Neet to be completed.
   """
   def snmp_get_next_index(table, rowIndex) when is_list(rowIndex),
     do: Mnesia.snmp_get_next_index(table, rowIndex)
 
   @doc """
-
+  TODO: Neet to be completed.
   """
   def snmp_get_row(table, rowIndex) when is_list(rowIndex),
     do: Mnesia.snmp_get_row(table, rowIndex)
 
   @doc """
-
+  TODO: Neet to be completed.
   """
   def snmp_open_table(table, snmp), do: Mnesia.snmp_open_table(table, snmp)
 end
